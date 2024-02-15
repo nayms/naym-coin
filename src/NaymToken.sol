9fe6500715821437808d0328c646930f4c092794
Ramesh Nair <ram@hiddentao.com>
2024-02-15 09:44:46 +0530

    chore: switch to solc 0.8.20 to be in sync with other repo

--- src/NaymToken.sol
+++ src/NaymToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPLv3
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.20;
 
 import { ERC20 } from "openzeppelin/token/ERC20/ERC20.sol";
 import { Ownable } from "openzeppelin/access/Ownable.sol";
