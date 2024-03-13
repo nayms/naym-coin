69d2ec98b94c6a9fa2665620ec58f32dab85b0b8
Kevin Park <kevin@fruitful.gg>
2024-03-13 20:46:20 +0700

    test: setup [wip]

--- test/NaymToken.t.sol
+++ test/NaymToken.t.sol
@@ -1,14 +1,25 @@
 // SPDX-License-Identifier: GPLv3
 pragma solidity ^0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
-import { Vm } from "forge-std/Vm.sol";
+import { Test, console as c, Vm } from "forge-std/Test.sol";
 
 import { NaymToken } from "../src/NaymToken.sol";
 import { IERC20Errors } from "openzeppelin/interfaces/draft-IERC6093.sol";
 import { Ownable } from "openzeppelin/access/Ownable.sol";
 
+import { IDiamondCut } from "lib/diamond-2-hardhat/contracts/interfaces/IDiamondCut.sol";
+import { DiamondProxy } from "src/generated/DiamondProxy.sol";
+import { IDiamondProxy } from "src/generated/IDiamondProxy.sol";
+import { LibDiamondHelper } from "src/generated/LibDiamondHelper.sol";
+import { LibGovernance } from "src/libs/LibGovernance.sol";
+import { LibHelpers } from "src/libs/LibHelpers.sol";
+import { InitDiamond } from "src/init/InitDiamond.sol";
+import { StdStyle } from "forge-std/StdStyle.sol";
+import { LibConstants as LC } from "src/libs/LibConstants.sol";
+
 contract NaymTokenTest is Test {
+    using StdStyle for *;
+
     NaymToken public t;
 
     address owner1 = address(0x111);
@@ -17,9 +28,90 @@ contract NaymTokenTest is Test {
     address minter2 = address(0x456);
     address user1 = address(0x1234);
 
+    address public naymsAddress;
+
+    IDiamondProxy public nayms;
+    InitDiamond public initDiamond;
+
+    address public deployer;
+    address public owner;
+    address public systemAdmin;
+
     function setUp() public {
         t = new NaymToken(owner1, minter1);
+
+        c.log("\n -- D01 Deployment Defaults\n");
+        c.log("block.chainid", block.chainid);
+
+        bool BOOL_FORK_TEST = vm.envOr({ name: "BOOL_FORK_TEST", defaultValue: false });
+        c.log("Are tests being run on a fork?".yellow().bold(), BOOL_FORK_TEST);
+        bool TESTS_FORK_UPGRADE_DIAMOND = vm.envOr({ name: "TESTS_FORK_UPGRADE_DIAMOND", defaultValue: true });
+        c.log("Are we testing diamond upgrades on a fork?".yellow().bold(), TESTS_FORK_UPGRADE_DIAMOND);
+
+        // if (BOOL_FORK_TEST) {
+        //     uint256 FORK_BLOCK = vm.envOr({
+        //         name: string.concat("FORK_BLOCK_", vm.toString(block.chainid)),
+        //         defaultValue: type(uint256).max
+        //     });
+        //     c.log("FORK_BLOCK", FORK_BLOCK);
+
+        //     if (FORK_BLOCK == type(uint256).max) {
+        //         c.log(
+        //             "Using latest block for fork, consider pinning a block number to avoid overloading the RPC
+        // endpoint"
+        //         );
+        //         vm.createSelectFork(getChain(block.chainid).rpcUrl);
+        //     } else {
+        //         vm.createSelectFork(getChain(block.chainid).rpcUrl, FORK_BLOCK);
+        //     }
+
+        //     naymsAddress = getDiamondAddress();
+        //     nayms = IDiamondProxy(naymsAddress);
+
+        //     deployer = address(this);
+        //     owner = nayms.owner();
+        //     vm.label(owner, "Owner");
+        //     systemAdmin = vm.envOr({
+        //         name: string.concat("SYSTEM_ADMIN_", vm.toString(block.chainid)),
+        //         defaultValue: address(0xE6aD24478bf7E1C0db07f7063A4019C83b1e5929)
+        //     });
+        //     systemAdminId = LibHelpers._getIdForAddress(systemAdmin);
+        //     vm.label(systemAdmin, "System Admin");
+
+        //     vm.startPrank(owner);
+        //     if (TESTS_FORK_UPGRADE_DIAMOND) {
+        //         IDiamondCut.FacetCut[] memory cut = LibDiamondHelper.deployFacetsAndGetCuts(naymsAddress);
+        //         scheduleAndUpgradeDiamond(cut);
+        //     }
+        // } else {
+        c.log("Local testing (no fork)");
+
+        deployer = address(this);
+        owner = address(this);
+        vm.startPrank(deployer);
+
+        vm.label(owner, "Account 0 (Test Contract address, deployer, owner)");
+        systemAdmin = makeAddr("System Admin 0");
+
+        c.log("Deploy diamond");
+        naymsAddress = address(new DiamondProxy(owner));
+        vm.label(naymsAddress, "Nayms diamond");
+        nayms = IDiamondProxy(naymsAddress);
+
+        // deploy all facets
+        IDiamondCut.FacetCut[] memory cuts = LibDiamondHelper.deployFacetsAndGetCuts(address(nayms));
+
+        initDiamond = new InitDiamond();
+        vm.label(address(initDiamond), "InitDiamond");
+        c.log("InitDiamond:", address(initDiamond));
+
+        c.log("Cut and init");
+        nayms.diamondCut(cuts, address(initDiamond), abi.encodeCall(InitDiamond.init, (systemAdmin)));
+
+        c.log("Diamond setup complete.");
     }
+
+    function test_diamond() public { }
 
     function test_Init() public {
         assertEq(t.name(), "Naym");
