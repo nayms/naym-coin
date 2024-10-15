d082bfbd836a6ad02ac098ffed162ad18fa0db07
Ramesh Nair <ram@hiddentao.com>
2024-10-15 16:47:08 +0700

    feat: update upgrade initialization code to use mapping

--- src/init/InitializationTest1.sol
+++ src/init/InitializationTest1.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
 import "../libs/LibERC20Token.sol";
 
 contract InitializationTest1 {
     function init() external {
+        uint256 initId = 1;
+
         AppStorage storage s = LibAppStorage.diamondStorage();
-        require(!s.initializationTest1, "InitializationTest already initialized");
+        require(!s.initComplete[initId], "Initialization already complete");
 
-        s.initializationTest1 = true;
+        s.initComplete[initId] = true;
 
+        // Burn the test tokens
         address testAddress = 0x5ba45268dC851209e66DEcD4E31e6723Fa3954C6;
         uint256 balance = LibERC20Token._balanceOf(testAddress);
         require(balance > 0, "No tokens to burn");
